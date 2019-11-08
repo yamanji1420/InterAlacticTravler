c8f54b500cf5144f6cb2e881d7a03c983859a120
yusra <yamanji1420@stu.d214.org>
2019-11-08 11:33:08 -0600

    added IBoutlet for thew image view on the second view controller

---
+++ InterAlacticTravler/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  InterAlacticTravler
+//
+//  Created by Yusra Amanji on 11/8/19.
+//  Copyright © 2019 John Hersey Highschool. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var starImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
